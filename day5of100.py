fa0e1cd94b21ddc9e6bfd5f17348b2e496673dde
Sarah Panaligan <spianodev@gmail.com>
2024-03-24 20:02:26 -0500

    created the first verticle tile

--- day5of100.py
+++ day5of100.py
@@ -23,7 +23,7 @@ def puzzle_tiles(width, height):
         middle = chr(95) + chr(124) + '     ' + chr(95) + chr(124) + '\n' + chr(40) + chr(95) + '   ' + chr(95) + ' ' + chr(40) + chr(95) + '\n'
         bottom = chr(124) + chr(95) + chr(95) + chr(40) + ' ' + chr(41) + chr(95) + chr(124)
         first_piece = " ".join([top, middle, bottom])
-        print(first_piece)
+        # print(first_piece, 'first piece')
         return first_piece
     def horizontal_tiles():
         top_row = '   ' + chr(95) + chr(40) + ' ' + chr(41) + chr(95) + chr(95)
@@ -45,14 +45,34 @@ def puzzle_tiles(width, height):
         horizontal_pieces = top_row + next_top_piece + second_row + next_second_row + third_row + next_third_row + \
                             last_row + next_last_row
         return horizontal_pieces
-    if width == 1 and height == 1:
+    def first_vertical_tile():
+        top_row = '\n' + ' ' + chr(124) + chr(95) + '     ' + chr(124) + chr(95) +  '\n'
+        middle_row = '  ' + chr(95) + chr(41) + ' ' + chr(95) + '   ' + chr(95) + chr(41) + '\n'
+        bottom_row = ' ' + chr(124) + chr(95) + chr(95) + chr(40) + ' ' + chr(41) + chr(95) + chr(124) + '\n'
+        first_vertical_piece = top_row + middle_row + bottom_row
+        return first_vertical_piece
+
+    if width > 1 and height == 1:
+        print(horizontal_tiles(), 'horizontal tiles')
+        return horizontal_tiles()
+    elif width == 1 and height > 1:
+        full_puzzle = str(initial_tile()) + str(first_vertical_tile())
+        # print(first_vertical_tile(), 'just the tile')
+        print(full_puzzle, 'vertical tiles')
+        return first_vertical_tile()
+    elif width == 1 and height == 1:
+        print('one tile')
         return initial_tile()
     else:
-        return horizontal_tiles()
+        full_puzzle = str(horizontal_tiles()) + str(first_vertical_tile())
+        print(full_puzzle)
+        return full_puzzle
 
 
 
-puzzle_tiles(6,1)
-
-'   _( )__ _( )__ _( )__\n _|     _|     _|     _|\n(_   _ (_   _ (_   _ (_\n |__( )_| _( )_| _( )_|' should equal
-'   _( )__ _( )__ _( )__\n _|     _|     _|     _|\n(_   _ (_   _ (_   _ (_\n |__( )_|__( )_|__( )_|\n |_     |_     |_     |_\n  _) _   _) _   _) _   _)\n |__( )_|__( )_|__( )_|'
+puzzle_tiles(1,5)
+#
+# '   _( )__ _( )__ _( )__\n _|     _|     _|     _|\n(_   _ (_   _ (_   _ (_\n |__( )_| _( )_| _( )_|' should equal
+# '   _( )__ _( )__ _( )__\n _|     _|     _|     _|\n(_   _ (_   _ (_   _ (_\n |__( )_|__( )_|__( )_|\n |_     |_     |_     |_\n  _) _   _) _   _) _   _)\n |__( )_|__( )_|__( )_|'
+# '|_     |_\n _) _   _)\n|__( )_|\n' should equal
+# '   _( )__ _( )__ _( )__\n _|     _|     _|     _|\n(_   _ (_   _ (_   _ (_\n |__( )_|__( )_|__( )_|\n |_     |_     |_     |_\n  _) _   _) _   _) _   _)\n |__( )_|__( )_|__( )_|'
