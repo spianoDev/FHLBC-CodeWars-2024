42d98cc8aa0b7cd7a1d7d9e82add9b66623099bb
Sarah Panaligan <spianodev@gmail.com>
2024-03-26 18:45:44 -0500

    corrected leading zero but the kata is not passing

--- day8of100.py
+++ day8of100.py
@@ -20,37 +20,50 @@
 ## This builds upon the class created in day 7, so I will be importing that file ##
 from day7of100 import Hero
 
+# def add_leading_zero(answer):
+#     if len(answer) == 1:
+#         return '0' + answer
+#     else:
+#         return answer
+
 def move(self, direction):
+    def add_leading_zero(answer):
+        if len(answer) == 1:
+            return '0' + answer
+        else:
+            return answer
     if direction == 'up' and int(self.position) < 10:
         raise ValueError(f'Direction {direction} is out of bounds. Player remains on position {self.position}')
-    elif direction == 'up' and int(self.position) > 10:
+    elif direction == 'up' and int(self.position) >= 10:
         self.position = str(int(self.position) - 10)
-        print(f'new position is {self.position}')
-        return self.position
-    if direction == 'down' and int(self.position) > 44:
+        print(f'new position is {add_leading_zero(self.position)}')
+        return add_leading_zero(self.position)
+    if direction == 'down' and int(self.position) > 34:
         raise ValueError(f'Direction {direction} is out of bounds. Player remains on position {self.position}')
     elif direction == 'down' and int(self.position) < 44:
         self.position = str(int(self.position) + 10)
-        print(f'new position is {self.position}')
-        return self.position
+        print(f'new position is {add_leading_zero(self.position)}')
+        return add_leading_zero(self.position)
     if direction == 'left' and int(self.position) % 10 == 0:
         raise ValueError(f'Direction {direction} is out of bounds. Player remains on position {self.position}')
     elif direction == 'left' and int(self.position) % 10 != 0:
         self.position = str(int(self.position) - 1)
-        print(f'new position is {self.position}')
-        return self.position
+        print(f'new position is {add_leading_zero(self.position)}')
+        return add_leading_zero(self.position)
     if direction == 'right' and (int(self.position) + 1) % 5 == 0:
         raise ValueError(f'Direction {direction} is out of bounds. Player remains on position {self.position}')
     elif direction == 'right' and (int(self.position) + 1) % 5 != 0:
         self.position = str(int(self.position) + 1)
-        print(f'new position is {self.position}')
-        return self.position
+        print(f'new position is {add_leading_zero(self.position)}')
+        return add_leading_zero(self.position)
 
 Hero.move = move
 
 myHero = Hero()
 # print(myHero)
-myHero.position = '21'
+myHero.position = '11'
 myHero.move('up')
 # myHero.move('down')
 # myHero.move('right')
+# myHero.move('up')
+# myHero.move('left')
